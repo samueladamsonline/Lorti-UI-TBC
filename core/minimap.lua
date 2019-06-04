4cceea488ea0798a2caac4bb74a49ff37f5c2ac6
Chordsy <joakim_elvin@live.se>
2019-06-04 23:06:06 +0200

    Update minimap.lua
    Removed the Custom made clock as the latest Classic Beta build includes a built-in clock at the bottom of the minimap.

--- core/minimap.lua
+++ core/minimap.lua
@@ -9,50 +9,13 @@ local zoom = Minimap:GetZoom()
     end
 end)
 
-
-
-    MiniMapTrackingFrame:SetFrameStrata'MEDIUM'
-    MiniMapTrackingFrame:ClearAllPoints()
-    MiniMapTrackingFrame:SetPoint('BOTTOM', -19, -19)
-    MiniMapTrackingFrame:SetScript('OnShow', function()
-        GameTimeFrame:ClearAllPoints() GameTimeFrame:SetPoint('BOTTOM', 33, 8)
-    end)
-    MiniMapTrackingFrame:SetScript('OnHide', function()
-        GameTimeFrame:ClearAllPoints() GameTimeFrame:SetPoint('BOTTOM', 12, 10)
-    end)
-
-    MiniMapMailFrame:ClearAllPoints()
-    MiniMapMailFrame:SetPoint('TOPRIGHT', 0, -10)
-
-    MinimapZoneText:ClearAllPoints()
-    MinimapZoneText:SetPoint('TOP', Minimap, 0, 17)
-
-    GameTimeFrame:SetScale(.76)
-    GameTimeFrame:ClearAllPoints() GameTimeFrame:SetPoint('BOTTOM', 12, 10)
-	
-	local frame = CreateFrame("Button", "Clock", UIParent)
-frame:ClearAllPoints()
-frame:SetFrameStrata("LOW")
-frame:SetWidth(165)
-frame:SetHeight(45)
-frame:SetPoint('BOTTOM', Minimap, 0, -30)
-frame.text = frame:CreateFontString("Status", "LOW", "GameFontNormal")
-frame.text:SetFont(STANDARD_TEXT_FONT, 16, "OUTLINE")
-frame.text:ClearAllPoints()
-frame.text:SetAllPoints(frame)
-frame.text:SetPoint('BOTTOM', Minimap, 0, -30)
-frame.text:SetFontObject(GameFontWhite)
-frame:SetScript("OnUpdate", function()
-frame.text:SetText(date("%H:%M"))
-end)
-
-    for i, v in pairs({
-        MinimapBorderTop,
-        MinimapToggleButton,
-		MiniMapWorldMapButton,
-		GameTimeFrame,
-        MinimapZoomIn,
-	    MinimapZoomOut
+for i, v in pairs({
+	MinimapBorderTop,
+	MinimapToggleButton,
+	MiniMapWorldMapButton,
+	GameTimeFrame,
+	MinimapZoomIn,
+	MinimapZoomOut
     }) do
         v:Hide()
     end
